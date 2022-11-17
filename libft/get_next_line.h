3df9be6627f27e297dd133caa8d09e9710e042c3
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-11-17 15:22:44 +0400

    get_next_line buffer size was 1?????

--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkhan <mkhan@student.42.fr>                +#+  +:+       +#+        */
+/*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 13:53:09 by mkhan             #+#    #+#             */
-/*   Updated: 2022/09/14 14:11:58 by mkhan            ###   ########.fr       */
+/*   Updated: 2022/11/17 15:22:27 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # include <unistd.h>
 # include "libft.h"
 
-# define BUFFER_SIZE 1
+# define BUFFER_SIZE 4096
 
 char	*get_next_line(int fd);
 char	*ft_gnl_strjoin(char *s1, char *s2);
