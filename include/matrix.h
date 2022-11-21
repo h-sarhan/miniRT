bd595c44eb5bd1faad9bcf3f574984c5ad1e4bab
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-11-21 11:52:33 +0400

    Matrix transpose

--- include/matrix.h
+++ include/matrix.h
@@ -6,18 +6,20 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 10:38:18 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/11/21 11:18:02 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/11/21 11:47:37 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MATRIX_H
 # define MATRIX_H
 
+# include <libft.h>
 # include <vector.h>
 
 typedef float	t_matrix[4][4];
 void	mat_multiply(t_matrix *res, const t_matrix *m1, const t_matrix *m2);
 void	mat_vec_multiply(t_vector *res, const t_matrix *mat,
 			const t_vector *vec);
+void	transpose_matrix(t_matrix *mat);
 
 #endif
