b2c10cfd86ec0669db80dbef804e4b4caf95ce8d
Mohammed ehsan ullah Khan <mkhan@lab2r2s7.42abudhabi.ae>
2022-11-21 20:46:18 +0400

    intersections array

--- include/intersection.h
+++ include/intersection.h
@@ -6,14 +6,16 @@
 /*   By: mkhan <mkhan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 12:19:12 by mkhan             #+#    #+#             */
-/*   Updated: 2022/11/21 12:54:02 by mkhan            ###   ########.fr       */
+/*   Updated: 2022/11/21 20:23:16 by mkhan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INTERSECTION_H
 # define INTERSECTION_H
 
-#include "../mlx/mlx.h"
+# include "../mlx/mlx.h"
+# include <ray.h>
+# include <shape.h>
 
 /**
  * @brief 
@@ -30,6 +32,22 @@ struct	s_mlx {
 	int		line_length;
 	int		endian;
 };
+
+typedef struct s_intersect t_intersect;
+
+struct s_intersect
+{	
+	float	time;
+	t_shape	*shape;
+};
+
+typedef struct s_intersections t_intersections;
+struct s_intersections
+{
+	t_intersect *arr;
+	int 		count;
+};
+
 
 void draw_scene(t_scene *scene);
 
