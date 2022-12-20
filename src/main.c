5ec97754974dc385d80623cb398b696ab1635740
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-12-20 17:08:24 +0400

    Done with this chapter

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 14:01:06 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/12/20 16:51:15 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/12/20 17:08:13 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ int	main(int argc, char **argv)
 	if (scene == NULL)
 		return (EXIT_FAILURE);
 	scene->render_scale = 1;
-	scene->edit_scale = 0.55;
+	scene->edit_scale = 0.35;
 	scene->render_w = 1920 * scene->render_scale;
 	scene->render_h = 1080 * scene->render_scale;
 	scene->edit_w = 1920 * scene->edit_scale;
@@ -68,7 +68,7 @@ int	main(int argc, char **argv)
 	scene->display_h = 1080 * 0.8;
 	scene->reflection_depth = REFLECTION_DEPTH;
 	
-	scene->collisions = false;
+	scene->collisions = true;
 	scene->shapes[0].highlighted = true;
 	scene->shapes[0].transparency = 1;
 	scene->shapes[0].reflectiveness = 1;
@@ -77,8 +77,6 @@ int	main(int argc, char **argv)
 	scene->shapes[1].reflectiveness = 1;
 	scene->shapes[1].ior = 1.0029;
 	
-	// scene->shapes[0].reflectiveness = 0;
-	// scene->shapes[1].reflectiveness = 0;
 	
 	sem_unlink("/loading");
 	scene->sem_loading = sem_open("/loading", O_CREAT, 0644, 0);
