9f53d3e028692b96647d0b1910efa53464c398d9
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-12-26 21:49:36 +0400

    Reverted mouse controls to rotate around x and y instead

--- src/edit_mode/mouse_controls.c
+++ src/edit_mode/mouse_controls.c
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 10:20:14 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/12/26 20:10:19 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/12/26 21:47:08 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,27 +66,24 @@ int	mouse_rotate(t_scene *scene)
 		#endif
 		if (abs(scene->mouse.x - scene->mouse.prev_x) > 1 || abs(scene->mouse.y - scene->mouse.prev_y) > 1)
 		{
-			float	x_diff = abs(scene->mouse.x - scene->mouse.prev_x);
-			float	y_diff = abs(scene->mouse.y - scene->mouse.prev_y);
-			float	degrees = max3((x_diff + y_diff) * 0.3, 3, 0);
-			t_vector	rot_dir;
-			rot_dir.x = scene->mouse.x - scene->mouse.prev_x;
-			rot_dir.y = scene->mouse.y - scene->mouse.prev_y;
-			rot_dir.z = 0;
-			rot_dir.w = 0;
-			rot_dir.x *= -1;
-			normalize_vec(&rot_dir);
-			t_vector	into;
-			ft_bzero(&into, sizeof(t_vector));
-			into.z = 1;
-			t_vector	rot_axis;
-			cross_product(&rot_axis, &into, &rot_dir);
-			normalize_vec(&rot_axis);
-			t_mat4	rot_mat;
-			axis_angle(&rot_mat, &rot_axis, deg_to_rad(degrees));
-			t_mat4	mat_copy;
-			ft_memcpy(&mat_copy, &scene->shapes[scene->shape_idx].added_rots, sizeof(t_mat4));
-			mat_multiply(&scene->shapes[scene->shape_idx].added_rots, &rot_mat, &mat_copy);
+			float	x_diff = abs(scene->mouse.x - scene->mouse.prev_x) * 0.2;
+			float	y_diff = abs(scene->mouse.y - scene->mouse.prev_y) * 0.2;
+			if (scene->mouse.prev_x < scene->mouse.x)
+			{
+				rotate_object_y(scene, &scene->shapes[scene->shape_idx], deg_to_rad(x_diff));
+			}
+			else if (scene->mouse.prev_x > scene->mouse.x)
+			{
+				rotate_object_y(scene, &scene->shapes[scene->shape_idx], -deg_to_rad(x_diff));
+			}
+			if (scene->mouse.prev_y < scene->mouse.y)
+			{
+				rotate_object_x(scene, &scene->shapes[scene->shape_idx], -deg_to_rad(y_diff));
+			}
+			else if (scene->mouse.prev_y > scene->mouse.y)
+			{
+				rotate_object_x(scene, &scene->shapes[scene->shape_idx], deg_to_rad(y_diff));
+			}
 			collide(scene, true, 100, &scene->shapes[scene->shape_idx]);
 			calculate_transforms(scene);
 			draw_scene(scene);
