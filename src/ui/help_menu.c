2c72ae1876d2fff4a688d3c136755fad7127cbfd
Hassan Sarhan <hassanAsarhan@outlook.com>
2023-02-19 19:54:13 +0400

    Working on norminetting help menu

--- src/ui/help_menu.c
+++ src/ui/help_menu.c
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 12:53:33 by hsarhan           #+#    #+#             */
-/*   Updated: 2023/01/18 15:47:45 by hsarhan          ###   ########.fr       */
+/*   Updated: 2023/02/19 19:51:18 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,209 +25,129 @@ void	show_help_menu(t_scene *scene)
 		x = scene->settings.disp_w * 0.1;
 		while (x < scene->settings.disp_w * 0.9)
 		{
-			*(int *)(scene->disp->disp_addr + ((y * scene->settings.disp_w) + x) * scene->disp->bpp) = 0x00;
+			*(int *)(scene->disp->disp_addr + ((y * scene->settings.disp_w) \
+				+ x) * scene->disp->bpp) = 0x00;
 			x++;
 		}
 		y++;
 	}
 }
 
+void	draw_cell(t_scene *scene, int row_num, int col_num, char *str)
+{
+	const int	col_gap = scene->settings.disp_w * 0.18;
+	const int	row_gap = scene->settings.disp_h * 0.028;
+
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		scene->settings.disp_w * 0.15 + col_gap * row_num,
+		scene->settings.disp_h * 0.23 + row_gap * col_num,
+		0xffffff, "- WASDQE: Translate shape"
+		);
+}
+
 void	draw_controls(t_scene *scene)
 {
+	const int	col_gap = scene->settings.disp_w * 0.18;
+	const int	row_gap = scene->settings.disp_h * 0.028;
+	int			i;
+
+	i = 0;
 	mlx_string_put(scene->disp->mlx, scene->disp->win,
 		(scene->settings.disp_w * 0.12),
 		(scene->settings.disp_h * 0.15),
 		0xffffff, "[HELP MENU]");
-	int i = 0;
-	int	col_gap = scene->settings.disp_w * 0.18;
-	int	row_gap = scene->settings.disp_h * 0.028;
-	i = 0;
 	mlx_string_put(scene->disp->mlx, scene->disp->win,
 		scene->settings.disp_w * 0.15 + col_gap * i,
 		scene->settings.disp_h * 0.215 + row_gap * 0,
-		0xffffff, "[EDIT MODE]"
-	);
+		0xffffff, "[EDIT MODE]");
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		scene->settings.disp_w * 0.15 + col_gap * i,
+		scene->settings.disp_h * 0.215 + row_gap * 0,
+		0xffffff, "[RENDER MODE]");
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		scene->settings.disp_w * 0.15 + col_gap * i,
+		scene->settings.disp_h * 0.215 + row_gap * 0,
+		0xffffff, "[CAMERA MODE]");
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		scene->settings.disp_w * 0.15 + col_gap * i,
+		scene->settings.disp_h * 0.215 + row_gap * 0,
+		0xffffff, "[LIGHT MODE]");
+	draw_cell(scene, 0, 1, "- WASDQE: Translate shape");
+	draw_cell(scene, 0, 2, "- +/-: Scale shape");
+	draw_cell(scene, 0, 3, "- +/- w/shift: Scale height");
+	draw_cell(scene, 0, 4, "- XYZ: Scale cube sides");
+	draw_cell(scene, 0, 5, "- Arrow keys: Rotate shape");
+	draw_cell(scene, 0, 6, "- Left click: Select shape");
+	draw_cell(scene, 0, 7, "- Mouse drag: Rotate shape");
+	draw_cell(scene, 0, 8, "- O: Look at");
+	draw_cell(scene, 0, 9, "- J: Anti Aliasing");
+	draw_cell(scene, 0, 10, "- Return: Spawn shape");
+	draw_cell(scene, 0, 11, "- T: Toggle shape");
+	draw_cell(scene, 0, 12, "- 1/2 3/4 5/6: Change colors");
+	draw_cell(scene, 0, 13, "- Space: Toggle edit mode");
+	draw_cell(scene, 0, 14, "- Tab: Switch shapes");
+	draw_cell(scene, 0, 15, "- R: Toggle reflections");
+	draw_cell(scene, 0, 16, "- L: Toggle light mode");
+	draw_cell(scene, 0, 17, "- C: Toggle Camera mode");
+	draw_cell(scene, 0, 18, "- H: Toggle help");
+	draw_cell(scene, 0, 19, "- ESC: Quit miniRT");
+	// col = 1
 	mlx_string_put(scene->disp->mlx, scene->disp->win,
 		scene->settings.disp_w * 0.15 + col_gap * i,
 		scene->settings.disp_h * 0.23 + row_gap * 1,
-		0xffffff, "- WASDQE: Translate shape"
-	);
+		0xffffff, "- Space: Toggle edit mode");
 	mlx_string_put(scene->disp->mlx, scene->disp->win,
 		scene->settings.disp_w * 0.15 + col_gap * i,
 		scene->settings.disp_h * 0.23 + row_gap * 2,
-		0xffffff, "- +/-: Scale shape"
-	);
+		0xffffff, "- ESC: Quit miniRT");
+	// col = 2
+
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		scene->settings.disp_w * 0.15 + col_gap * i,
+		scene->settings.disp_h * 0.23 + row_gap * 1,
+		0xffffff, "- WASDQE: Move camera");
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		scene->settings.disp_w * 0.15 + col_gap * i,
+		scene->settings.disp_h * 0.23 + row_gap * 2,
+		0xffffff, "- Arrow Keys: Orient Camera");
 	mlx_string_put(scene->disp->mlx, scene->disp->win,
 		scene->settings.disp_w * 0.15 + col_gap * i,
 		scene->settings.disp_h * 0.23 + row_gap * 3,
-		0xffffff, "- +/- w/shift: Scale height"
-	);
+		0xffffff, "- C: Toggle Camera mode");
 	mlx_string_put(scene->disp->mlx, scene->disp->win,
 		scene->settings.disp_w * 0.15 + col_gap * i,
 		scene->settings.disp_h * 0.23 + row_gap * 4,
-		0xffffff, "- XYZ: Scale cube sides"
-	);
+		0xffffff, "- Space: Toggle edit mode");
 	mlx_string_put(scene->disp->mlx, scene->disp->win,
 		scene->settings.disp_w * 0.15 + col_gap * i,
 		scene->settings.disp_h * 0.23 + row_gap * 5,
-		0xffffff, "- Arrow keys: Rotate shape"
-	);
+		0xffffff, "- ESC: Quit miniRT");
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		scene->settings.disp_w * 0.15 + col_gap * i,
+		scene->settings.disp_h * 0.23 + row_gap * 1,
+		0xffffff, "- WASDQE: Move light");
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		scene->settings.disp_w * 0.15 + col_gap * i,
+		scene->settings.disp_h * 0.23 + row_gap * 2,
+		0xffffff, "- +/-: Light intensity");
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		scene->settings.disp_w * 0.15 + col_gap * i,
+		scene->settings.disp_h * 0.23 + row_gap * 3,
+		0xffffff, "- +/- w/shift: Light beam width");
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		scene->settings.disp_w * 0.15 + col_gap * i,
+		scene->settings.disp_h * 0.23 + row_gap * 4,
+		0xffffff, "- TAB: Switch lights");
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		scene->settings.disp_w * 0.15 + col_gap * i,
+		scene->settings.disp_h * 0.23 + row_gap * 5,
+		0xffffff, "- C: Toggle Camera mode");
 	mlx_string_put(scene->disp->mlx, scene->disp->win,
 		scene->settings.disp_w * 0.15 + col_gap * i,
 		scene->settings.disp_h * 0.23 + row_gap * 6,
-		0xffffff, "- Left click: Select shape"
-	);
+		0xffffff, "- Space: Toggle edit mode");
 	mlx_string_put(scene->disp->mlx, scene->disp->win,
 		scene->settings.disp_w * 0.15 + col_gap * i,
 		scene->settings.disp_h * 0.23 + row_gap * 7,
-		0xffffff, "- Mouse drag: Rotate shape"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 8,
-		0xffffff, "- O: Look at"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 9,
-		0xffffff, "- J: Anti Aliasing"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 10,
-		0xffffff, "- Return: Spawn shape"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 11,
-		0xffffff, "- T: Toggle shape"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 12,
-		0xffffff, "- 1/2 3/4 5/6: Change colors"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 13,
-		0xffffff, "- Space: Toggle edit mode"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 14,
-		0xffffff, "- Tab: Switch shapes"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 15,
-		0xffffff, "- R: Toggle reflections"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 16,
-		0xffffff, "- L: Toggle light mode"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 17,
-		0xffffff, "- C: Toggle Camera mode"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 18,
-		0xffffff, "- H: Toggle help"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 19,
-		0xffffff, "- ESC: Quit miniRT"
-	);
-	i++;
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.215 + row_gap * 0,
-		0xffffff, "[RENDER MODE]"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 1,
-		0xffffff, "- Space: Toggle edit mode"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 2,
-		0xffffff, "- ESC: Quit miniRT"
-	);
-	i++;
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.215 + row_gap * 0,
-		0xffffff, "[CAMERA MODE]"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 1,
-		0xffffff, "- WASDQE: Move camera"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 2,
-		0xffffff, "- Arrow Keys: Orient Camera"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 3,
-		0xffffff, "- C: Toggle Camera mode"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 4,
-		0xffffff, "- Space: Toggle edit mode"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 5,
-		0xffffff, "- ESC: Quit miniRT"
-	);
-	i++;
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.215 + row_gap * 0,
-		0xffffff, "[LIGHT MODE]"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 1,
-		0xffffff, "- WASDQE: Move light"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 2,
-		0xffffff, "- +/-: Light intensity"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 3,
-		0xffffff, "- +/- w/shift: Light beam width"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 4,
-		0xffffff, "- TAB: Switch lights"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 5,
-		0xffffff, "- C: Toggle Camera mode"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 6,
-		0xffffff, "- Space: Toggle edit mode"
-	);
-	mlx_string_put(scene->disp->mlx, scene->disp->win,
-		scene->settings.disp_w * 0.15 + col_gap * i,
-		scene->settings.disp_h * 0.23 + row_gap * 7,
-		0xffffff, "- ESC: Quit miniRT"
-	);
+		0xffffff, "- ESC: Quit miniRT");
 }
--- src/ui/shape_info.c
+++ src/ui/shape_info.c
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 17:32:41 by hsarhan           #+#    #+#             */
-/*   Updated: 2023/02/05 21:04:53 by hsarhan          ###   ########.fr       */
+/*   Updated: 2023/02/19 19:26:33 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,144 +19,132 @@ void	show_light_banner(t_scene *scene)
 		0xffffff, "LIGHT MODE");
 }
 
-// NORM LATER
-// Planning to change this significantly so I will norm this later
-// Will need to write ftoa to avoid using sprintf
+void	draw_shape_type(t_scene *scene, t_shape *shape, t_vector *origin_proj)
+{
+	if (shape->type == SPHERE)
+		mlx_string_put(scene->disp->mlx, scene->disp->win,
+			(origin_proj->x * scene->settings.disp_w),
+			(origin_proj->y - 0.12) * scene->settings.disp_h,
+			0xffffff, "Sphere");
+	if (shape->type == CYLINDER)
+		mlx_string_put(scene->disp->mlx, scene->disp->win,
+			(origin_proj->x * scene->settings.disp_w),
+			(origin_proj->y - 0.12) * scene->settings.disp_h,
+			0xffffff, "Cylinder");
+	if (shape->type == CUBE)
+		mlx_string_put(scene->disp->mlx, scene->disp->win,
+			(origin_proj->x * scene->settings.disp_w),
+			(origin_proj->y - 0.12) * scene->settings.disp_h,
+			0xffffff, "Cube");
+	if (shape->type == CONE)
+		mlx_string_put(scene->disp->mlx, scene->disp->win,
+			(origin_proj->x * scene->settings.disp_w),
+			(origin_proj->y - 0.12) * scene->settings.disp_h,
+			0xffffff, "Cone");
+}
+
+void	draw_shape_coordinates(t_shape *shape, t_scene *scene,
+			t_vector *origin_proj)
+{
+	char	*str;
+
+	str = ftoa(shape->origin.x);
+	str = ft_strjoin_free(" x: ", str, 2);
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		(origin_proj->x * scene->settings.disp_w),
+		(origin_proj->y - 0.10) * scene->settings.disp_h,
+		0xffffff, str);
+	free(str);
+	str = ftoa(shape->origin.y);
+	str = ft_strjoin_free(" y: ", str, 2);
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		(origin_proj->x * scene->settings.disp_w),
+		(origin_proj->y - 0.08) * scene->settings.disp_h,
+		0xffffff, str);
+	free(str);
+	str = ftoa(shape->origin.z);
+	str = ft_strjoin_free(" z: ", str, 2);
+	mlx_string_put(scene->disp->mlx, scene->disp->win,
+		(origin_proj->x * scene->settings.disp_w),
+		(origin_proj->y - 0.06) * scene->settings.disp_h,
+		0xffffff, str);
+	free(str);
+}
+
+void	draw_shape_properties(t_shape *shape, t_scene *scene,
+			t_vector *origin_proj)
+{
+	char	*str;
+
+	if (shape->type == SPHERE || shape->type == CONE || shape->type == CYLINDER)
+	{
+		str = ftoa(shape->props.radius);
+		str = ft_strjoin_free(" radius: ", str, 2);
+		mlx_string_put(scene->disp->mlx, scene->disp->win,
+			(origin_proj->x * scene->settings.disp_w),
+			(origin_proj->y - 0.04) * scene->settings.disp_h,
+			0xffffff, str);
+		free(str);
+	}
+	if (shape->type == CYLINDER || shape->type == CONE)
+	{
+		str = ftoa(shape->props.height);
+		str = ft_strjoin_free(" height: ", str, 2);
+		mlx_string_put(scene->disp->mlx, scene->disp->win,
+			(origin_proj->x * scene->settings.disp_w),
+			(origin_proj->y - 0.02) * scene->settings.disp_h,
+			0xffffff, str);
+		free(str);
+	}
+}
+
+t_vector	get_projected_origin(t_shape *shape, t_scene *scene)
+{
+	t_vector	origin;
+	t_vector	orient;
+	t_vector	origin_proj;
+
+	origin = shape->origin;
+	origin.x -= 0.2;
+	if (shape->type == SPHERE || shape->type == CYLINDER)
+		origin.y += shape->props.radius;
+	if (shape->type == CONE)
+		origin.y += shape->props.height / 2;
+	if (shape->type == CUBE)
+	{
+		mat_vec_multiply(&orient, &shape->added_rots, &shape->orientation);
+		origin.y += shape->props.scale.y * fabs(orient.y) + \
+			shape->props.scale.x * fabs(orient.x) + shape->props.scale.z * \
+			fabs(orient.z);
+	}
+	mat_vec_multiply(&origin_proj, &scene->cam.transform, &origin);
+	return (origin_proj);
+}
+
 void	draw_shape_info(t_scene *scene)
 {
 	int			shape_idx;
 	t_shape		*shape;
 	t_vector	origin_proj;
-	t_vector	origin;
-	char		*str;
 
-	shape_idx = 0;
+	shape_idx = -1;
 	if (scene->settings.edit_mode == false)
 		return ;
-	while (shape_idx < scene->count.shapes)
+	while (++shape_idx < scene->count.shapes)
 	{
 		shape = &scene->shapes[shape_idx];
 		if (shape->props.highlighted == false)
-		{
-			shape_idx++;
 			continue ;
-		}
-		origin = shape->origin;
-		origin.x -= 0.2;
-		if (shape->type == SPHERE || shape->type == CYLINDER)
-			origin.y += shape->props.radius;
-		if (shape->type == CONE)
-			origin.y += shape->props.height / 2;
-		if (shape->type == CUBE)
-		{
-			t_vector current_orientation;
-			mat_vec_multiply(&current_orientation, &shape->added_rots, &shape->orientation);
-			origin.y += shape->props.scale.y * fabs(current_orientation.y) + shape->props.scale.x * fabs(current_orientation.x) + shape->props.scale.z * fabs(current_orientation.z);
-		}
-		mat_vec_multiply(&origin_proj, &scene->cam.transform, &origin);
+		origin_proj = get_projected_origin(shape, scene);
 		if (origin_proj.z > 0)
 			return ;
 		perspective_projection(&origin_proj, scene);
 		if (shape->type == SPHERE || shape->type == CYLINDER
 			|| shape->type == CUBE || shape->type == CONE)
 		{
-			if (shape->type == SPHERE)
-				mlx_string_put(scene->disp->mlx, scene->disp->win,
-					(origin_proj.x * scene->settings.disp_w),
-					(origin_proj.y - 0.12) * scene->settings.disp_h,
-					0xffffff, "Sphere");
-			if (shape->type == CYLINDER)
-				mlx_string_put(scene->disp->mlx, scene->disp->win,
-					(origin_proj.x * scene->settings.disp_w),
-					(origin_proj.y - 0.12) * scene->settings.disp_h,
-					0xffffff, "Cylinder");
-			if (shape->type == CUBE)
-				mlx_string_put(scene->disp->mlx, scene->disp->win,
-					(origin_proj.x * scene->settings.disp_w),
-					(origin_proj.y - 0.12) * scene->settings.disp_h,
-					0xffffff, "Cube");
-			if (shape->type == CONE)
-				mlx_string_put(scene->disp->mlx, scene->disp->win,
-					(origin_proj.x * scene->settings.disp_w),
-					(origin_proj.y - 0.12) * scene->settings.disp_h,
-					0xffffff, "Cone");
-			str = ftoa(shape->origin.x);
-			str = ft_strjoin_free(" x: ", str, 2);
-			mlx_string_put(scene->disp->mlx, scene->disp->win,
-				(origin_proj.x * scene->settings.disp_w),
-				(origin_proj.y - 0.10) * scene->settings.disp_h,
-				0xffffff, str);
-			free(str);
-			str = ftoa(shape->origin.y);
-			str = ft_strjoin_free(" y: ", str, 2);
-			mlx_string_put(scene->disp->mlx, scene->disp->win,
-				(origin_proj.x * scene->settings.disp_w),
-				(origin_proj.y - 0.08) * scene->settings.disp_h,
-				0xffffff, str);
-			free(str);
-			str = ftoa(shape->origin.z);
-			str = ft_strjoin_free(" z: ", str, 2);
-			mlx_string_put(scene->disp->mlx, scene->disp->win,
-				(origin_proj.x * scene->settings.disp_w),
-				(origin_proj.y - 0.06) * scene->settings.disp_h,
-				0xffffff, str);
-			free(str);
-			if (shape->type == SPHERE || shape->type == CONE || shape->type == CYLINDER)
-			{
-				str = ftoa(shape->props.radius);
-				str = ft_strjoin_free(" radius: ", str, 2);
-				mlx_string_put(scene->disp->mlx, scene->disp->win,
-					(origin_proj.x * scene->settings.disp_w),
-					(origin_proj.y - 0.04) * scene->settings.disp_h,
-					0xffffff, str);
-				free(str);
-			}
-			if (shape->type == CYLINDER || shape->type == CONE)
-			{
-				str = ftoa(shape->props.height);
-				str = ft_strjoin_free(" height: ", str, 2);
-				mlx_string_put(scene->disp->mlx, scene->disp->win,
-					(origin_proj.x * scene->settings.disp_w),
-					(origin_proj.y - 0.02) * scene->settings.disp_h,
-					0xffffff, str);
-				free(str);
-			}
+			draw_shape_type(scene, shape, &origin_proj);
+			draw_shape_coordinates(shape, scene, &origin_proj);
+			draw_shape_properties(shape, scene, &origin_proj);
 		}
-		shape_idx++;
 	}
 }
-
-// ! Not currently using but could be helpful later
-// void	dda(t_scene *scene, double x1, double x2, double y1, double y2, int color)
-// {
-// 	double	dy;
-// 	double	dx;
-// 	double	steps;
-// 	int		i;
-// 	char	*dst;
-
-// 	dx = (x2 - x1);
-// 	dy = (y2 - y1);
-// 	if ((fabs(x1 - x2) + fabs(y1 - y2)) < 2)
-// 	{
-// 		return ;
-// 	}
-// 	if (fabs(dx) > fabs(dy))
-// 		steps = fabs(dx);
-// 	else
-// 		steps = fabs(dy);
-// 	i = 0;
-// 	dx /= steps;
-// 	dy /= steps;
-// 	while (i <= steps)
-// 	{
-// 		if (y1 < scene->settings.disp_h && x1 < scene->settings.disp_w && x1 >= 0 && y1 >= 0)
-// 		{
-// 			dst = scene->disp->disp_addr + (int)((int)y1 * scene->settings.disp_w + (int)x1) * scene->disp->bpp;
-// 			*(unsigned int*)dst = color;
-// 		}
-// 		y1 += dy;
-// 		x1 += dx;
-// 		i++;
-// 	}
-// }
